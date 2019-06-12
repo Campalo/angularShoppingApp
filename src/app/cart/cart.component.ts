c40cef154919cee76bcc319ceec6b08921fb5008
Campalo <25295489+Campalo@users.noreply.github.com>
2019-06-12 12:41:25 +0200

    add customer data to firebase db

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
-import { FormBuilder } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from '../cart.service';
+import {FormBuilder} from '@angular/forms';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  //define LOCAL itemList property 
+  //define LOCAL itemList property
   itemList;
   // define the checkoutForm property to store the form model
   checkoutForm;
@@ -17,26 +18,27 @@ export class CartComponent implements OnInit {
     // use the service to be able to display the items inside the cart
     private cartService: CartService,
     private formBuilder: FormBuilder,
-  ) {
-    // set the checkoutForm property with a form model
-    this.checkoutForm = this.formBuilder.group({
-      name: '',
-      address: ''
-    });
-  }
+    private db: AngularFirestore,
+  ) {}
 
   ngOnInit() {
     // assign a new value to itemList by returning a copy if the Items array
     this.itemList = this.cartService.getItems();
+    // set the checkoutForm property with a form model
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    });
   }
 
   onSubmit(customerData) {
-    // process checkout data from the form 
+    // process checkout data from the form
     console.warn('Your order has been submitted', customerData);
+    // add name and adress from the customer to customerData db on firebase
+    this.db.collection('customerData').add(customerData);
     //clear the cart
     this.itemList = this.cartService.clearCart();
     //reset the form
     this.checkoutForm.reset();
   }
-
 }
