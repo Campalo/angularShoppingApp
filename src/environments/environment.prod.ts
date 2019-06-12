9838bc131e9c4f7b670ee3f46547c3017b2b1909
Campalo <25295489+Campalo@users.noreply.github.com>
2019-06-12 15:59:18 +0200

    add the firebase config inside the environement.prod to build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
+// firebase configuration for the build done by Netlify
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDH2bxFx5G13Hw_U6zWN3G6TFcD8df_Lew',
+    authDomain: 'ng-shopping-app-5367a.firebaseapp.com',
+    databaseURL: 'https://ng-shopping-app-5367a.firebaseio.com',
+    projectId: 'ng-shopping-app-5367a',
+    storageBucket: 'ng-shopping-app-5367a.appspot.com',
+    messagingSenderId: '771346096116',
+    appId: '1:771346096116:web:2c7ce5c9d989b9cf',
+  },
 };
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
